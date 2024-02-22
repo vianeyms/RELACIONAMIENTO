cab2e0d96329bf9d872ea9006a0db1b38fe68f39
vianeyms <ma090454@uaeh.edu.mx>
2024-02-22 16:26:15 -0600

    ÁLGEBRA LINEAL EN R
    Se han hecho prácticas con álgebra matricial para lograr identificar las correlaciones y ocurrencia

--- Archivo lab 1.R
+++ Archivo lab 1.R
@@ -26,54 +26,38 @@ M = as.matrix(
            header = T, 
            row.names = 1))
 
+M
 
-
+dim (M)
 # Es la estructura de red que AMAZON ocupa para hacer recomendaciones en base a tus compras actuales
 
-
+M [1:6,1]
 # Ver s?lo el vector de clientes que compran el producto "corbata" (producto de la columna 1)
-
+M[1:6,1:2]
 
 #Ver s?lo un fragmento de la base original, en relaci?n a los dos productos que me interesan
 #Puede ser ?til para explorar clientes y productos
-
+rowSums(M)
 
 # Computar grado de centralidad
+t(M)
 
+M+M
 
-# funci?n ?til para enfocarnos en productos y no s?lo en clientes
+X = M+M
 
+M^2
 
-# Suma de matrices
+M * M
 
+M %*% t(M)
 
+t(M) %*% M
 
-# Crear nuevo un objeto llamado x
+P = t (M) %*% M
 
+diag (P) = 0
 
+C= M %*% t(M)
 
-#multiplicaci?n de matrices checar tama?os
-#checar tama?o de la matriz -dim-
-
-
-
-#El algoritmo de recomendaci?n de AMAZON, hace b?sicamente lo anterior.
-# para tener el n?mero de veces que 2 productos han sido comprados por el mismo cliente
-#identificar productos que van frecuentemente juntos (co ocurrencias par)
-
-
-
-#similaridad de productos (de gustos) matriz de co ocurrencia de productos
-
-
-
-
-#no es una matriz identidad (como en la correlaci?n) 
-
-
-
-#similaridad de clientes - matriz de co ocurrencia de clienes
-
-
-
-#no es una matriz identidad (como en la correlaci?n) 
+diag (C)=0
